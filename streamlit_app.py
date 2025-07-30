e6cc821773db902f7cc65e6de159eba55511de9f
Nokuthula-collab <nokuthula.ntungo@gmail.com>
2025-07-30 21:40:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ if ingredients_list:
         st.success("✅ Your Smoothie is ordered, " + name_on_order + "!")
 
 import requests
-# smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # st.write(f"Status code: {smoothiefroot_response.status_code}")
-# st.text(smoothiefroot_response.json())
-# sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
